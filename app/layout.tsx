a99d54de0ec12128c4a065c46e7da45b3fcf1256
footballiq-analytics <8nkwjvb266@privaterelay.appleid.com>
2026-09-07 23:18:19 +0300

    Load professional team builder theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./fantasy.css";
+import "./pro-team.css";
 import { BottomNav, Footer, Header } from "@/components/site-shell";
 import { ThemeProvider } from "@/components/theme-provider";
 
