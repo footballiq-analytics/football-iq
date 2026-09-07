8e2259a7a944c36e32b766c1024c65b2bfff130d
footballiq-analytics <8nkwjvb266@privaterelay.appleid.com>
2026-09-08 02:15:53 +0300

    Load team data filter styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import "./fantasy.css";
 import "./pro-team.css";
 import "./form-colors.css";
+import "./team-data.css";
 import { BottomNav, Footer, Header } from "@/components/site-shell";
 import { ThemeProvider } from "@/components/theme-provider";
 
