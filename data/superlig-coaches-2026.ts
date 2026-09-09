b26ec3100ec7a8a208cf0a419ad806421977b9b7
footballiq-analytics <8nkwjvb266@privaterelay.appleid.com>
2026-09-09 14:52:54 +0300

    Add 2026-27 Super Lig coach directory

---
+++ data/superlig-coaches-2026.ts
@@ -0,0 +1,28 @@
+export type FantasyCoach = {
+  id: string;
+  name: string;
+  club: string;
+  country: string;
+};
+
+// 2026-27 sezon başlangıcı teknik direktör listesi. Kaynak: AA, 10 Ağustos 2026.
+export const SUPER_LIG_COACHES_2026_27: FantasyCoach[] = [
+  { id: "coach-amed", name: "Besnik Hasi", club: "Amed Sportif Faaliyetler", country: "Arnavutluk" },
+  { id: "coach-besiktas", name: "Vincenzo Italiano", club: "Beşiktaş", country: "İtalya" },
+  { id: "coach-alanyaspor", name: "João Pereira", club: "Corendon Alanyaspor", country: "Portekiz" },
+  { id: "coach-rizespor", name: "Recep Uçar", club: "Çaykur Rizespor", country: "Türkiye" },
+  { id: "coach-corum", name: "Uğur Uçar", club: "Arca Çorum FK", country: "Türkiye" },
+  { id: "coach-erzurum", name: "Serkan Özbalta", club: "Erzurumspor FK", country: "Türkiye" },
+  { id: "coach-eyup", name: "Özhan Pulat", club: "Eyüpspor", country: "Türkiye" },
+  { id: "coach-fenerbahce", name: "İsmail Kartal", club: "Fenerbahçe", country: "Türkiye" },
+  { id: "coach-galatasaray", name: "Okan Buruk", club: "Galatasaray", country: "Türkiye" },
+  { id: "coach-gaziantep", name: "Mirel Rădoi", club: "Gaziantep FK", country: "Romanya" },
+  { id: "coach-genclerbirligi", name: "Metin Diyadin", club: "Gençlerbirliği", country: "Türkiye" },
+  { id: "coach-goztepe", name: "Stanimir Stoilov", club: "Göztepe", country: "Bulgaristan" },
+  { id: "coach-basaksehir", name: "Nuri Şahin", club: "İstanbul Başakşehir FK", country: "Türkiye" },
+  { id: "coach-kasimpasa", name: "Emre Belözoğlu", club: "Kasımpaşa", country: "Türkiye" },
+  { id: "coach-kocaeli", name: "Selçuk İnan", club: "Kocaelispor", country: "Türkiye" },
+  { id: "coach-konya", name: "İlhan Palut", club: "Tümosan Konyaspor", country: "Türkiye" },
+  { id: "coach-samsun", name: "Thomas Reis", club: "Samsunspor", country: "Almanya" },
+  { id: "coach-trabzon", name: "Fatih Tekke", club: "Trabzonspor", country: "Türkiye" },
+];
