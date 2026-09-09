2c884f22b2128c7e9c032610efde0cfa2b333d30
footballiq-analytics <8nkwjvb266@privaterelay.appleid.com>
2026-09-09 09:47:07 +0300

    Make ESLint Next config compatible with current export shape

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,9 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals.js";
-export default defineConfig([...nextVitals, globalIgnores([".next/**", "node_modules/**", "out/**"])]);
+
+const nextConfig = Array.isArray(nextVitals) ? nextVitals : [nextVitals];
+
+export default defineConfig([
+  ...nextConfig,
+  globalIgnores([".next/**", "node_modules/**", "out/**"]),
+]);
